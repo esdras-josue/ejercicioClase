c8a13568d5a70abb328522df9db8e982683ef54a
esdras-josue <esdraspea@gmail.com>
2024-07-19 18:41:22 -0600

    Add project files.

---
+++ ejercicioClase/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioClase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Para cada uno de los atributos usted definirá el tipo de dato de acuerdo
+            a lo que se solicita mostrar la información.
+            Deberá mostrar 3 superhéroes con todos los atributos, 
+            para cada superhéroe deberá crear un objeto ingresando los datos
+            */
+
+            SuperHeroe spiderman = new SuperHeroe();
+            SuperHeroe batman = new SuperHeroe();
+            SuperHeroe ironMan = new SuperHeroe();
+
+            spiderman.Nombre = "Spiderman";
+            spiderman.IdentidadSecreta = "Peter Parker";
+            spiderman.Ciudad = "New York";
+            spiderman.PuedeVolar = false;
+            spiderman.Poder.Nombre = "Super fuerza";
+            spiderman.Poder.Descripcion = "Puede levantar objetos muy pesados";
+            spiderman.Poder.Nivel = 9;
+            spiderman.Imprimir();
+
+            Console.WriteLine();
+           
+            batman.Nombre = "Batman";
+            batman.IdentidadSecreta = "Bruce Wayne";
+            batman.Ciudad = "Gotham City";
+            batman.PuedeVolar = false;
+            batman.Poder.Nombre = "Super Fuerza";
+            batman.Poder.Descripcion = "Puede levantar objetos muy pesados";
+            batman.Poder.Nivel = 8;
+            batman.Imprimir();
+
+            Console.WriteLine();
+     
+            ironMan.Nombre = "IronMan";
+            ironMan.IdentidadSecreta = "Tony Star";
+            ironMan.Ciudad = "New York";
+            ironMan.PuedeVolar = true;
+            ironMan.Poder.Nombre = "Super Fuerza";
+            ironMan.Poder.Descripcion = "Puede levantar objetos muy pesados";
+            ironMan.Poder.Nivel = 10;
+            ironMan.Imprimir();
+
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
---
+++ ejercicioClase/SuperHeroe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioClase
+{
+    public class SuperHeroe
+    {
+       
+        public string Nombre {  get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad { get; set; }
+        public bool PuedeVolar { get; set; }
+        public SuperPoder Poder { get; set; }
+
+        public SuperHeroe()
+        {
+            Poder = new SuperPoder();
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede vodar:{(PuedeVolar ? " Si": "No")}");
+            Console.WriteLine($"Super poder: {Poder.Nombre}");
+            Console.WriteLine($"Descripcion: {Poder.Descripcion}");
+            Console.WriteLine($"Nivel: {Poder.Nivel}"); 
+        }
+    }
+}
